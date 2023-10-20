3024dcaa4e3098fdea33dd90f82b0a8f5579764d
Felix <felixdnbox@gmail.com>
2023-10-20 22:36:45 +0300

    20.10.2023

--- Order/Authorization_Registration/models.py
+++ Order/Authorization_Registration/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    login = models.CharField(max_length=255)
+    password = models.IntegerField()
+    email = models.CharField(max_length=255)
+    phone = models.IntegerField()
